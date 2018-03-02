41352e2d952c3250247a0d39a2efa27bc2b57177
JM Delgado <jmigueldelgado@gmail.com>
2018-03-02 14:19:54 +0100

    ogr2ogr subprocess call

--- callogr2ogr.py
+++ callogr2ogr.py
@@ -4,5 +4,5 @@ import modules.getPaths
 
 postgis_user
 postgis_pass
-
-subprocess.call(['ogr2ogr','-f','"PostgreSQL"',"-f","GML",polOut + "/" + out_gml])
+pg='PG:"host=localhost dbname=watermasks user=' + postgis_user + ' password=' + postgis_pass + '"'
+subprocess.call(['ogr2ogr','-f','PostgreSQL',pg,'/home/delgado/load_to_postgis','-nln','masks','-append'])
