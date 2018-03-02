e11a9d4b0b49663dcbb4bd9b5666035ee26e284d
JM Delgado <jmigueldelgado@gmail.com>
2018-03-02 18:34:59 +0100

    attempting to correct ogr2ogr

--- callogr2ogr.py
+++ callogr2ogr.py
@@ -1,5 +1,5 @@
 import subprocess
 from  modules.getPaths import *
 
-pg='PG:"host=localhost dbname=watermasks user=' + postgis_user + ' password=' + postgis_pass + '"'
+pg='PG:"dbname=watermasks user=' + postgis_user + ' password=' + postgis_pass + '"'
 subprocess.call(['ogr2ogr','-f','PostgreSQL',pg,'/home/delgado/load_to_postgis/latest.geojson','-nln','masks','-append'])
