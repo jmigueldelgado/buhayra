16ba2cbc5feaf886d77cc418ce251fedcb625dc7
JM Delgado <jmigueldelgado@gmail.com>
2018-03-02 18:53:19 +0100

    ignoring ogr2ogr

--- callogr2ogr.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-from  modules.getPaths import *
-
-pg='PG:"dbname=watermasks user=' + postgis_user + ' password=' + postgis_pass + '"'
-subprocess.call(['ogr2ogr','-f','PostgreSQL',pg,'/home/delgado/load_to_postgis/latest.geojson','-nln','masks','-append'])
