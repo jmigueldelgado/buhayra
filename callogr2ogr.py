0514d809e6f29d07ca993fb9c504821bbfd27a3c
JM Delgado <jmigueldelgado@gmail.com>
2018-03-02 18:19:04 +0100

    forgot to add the filename to import

--- callogr2ogr.py
+++ callogr2ogr.py
@@ -2,4 +2,4 @@ import subprocess
 from  modules.getPaths import *
 
 pg='PG:"host=localhost dbname=watermasks user=' + postgis_user + ' password=' + postgis_pass + '"'
-subprocess.call(['ogr2ogr','-f','PostgreSQL',pg,'/home/delgado/load_to_postgis','-nln','masks','-append'])
+subprocess.call(['ogr2ogr','-f','PostgreSQL',pg,'/home/delgado/load_to_postgis/latest.geojson','-nln','masks','-append'])
