071996b14c01007758b1d4acf249fbebed2730d5
JM Delgado <jmigueldelgado@gmail.com>
2018-03-08 19:11:38 +0100

    moves write command away from the function

--- modules/filter_vct.R
+++ modules/filter_vct.R
@@ -1,6 +1,5 @@
 require(sf)
 require(dplyr)
-
 library(lubridate)
 library(reshape2)
 orson=TRUE
@@ -11,6 +10,61 @@ if(orson)
 {
     scratch="/home/delgado/scratch"
 }
+
+join_ids_with_feats <- function(ids,psimpl) {
+    out <- tryCatch(
+        {
+            # Just to highlight: if you want to use more than one
+            # R expression in the "try" part then you'll have to
+            # use curly brackets.
+            # 'tryCatch()' will return the last evaluated expression
+            # in case the "try" part was completed successfully
+
+            message("This is the 'try' part")
+
+            pfilter <- left_join(ids,psimpl) %>%
+                st_as_sf %>%
+                group_by(id_cogerh) %>%
+                summarize(source_id=as.integer(first(source_id)),ingestion_time=first(ingestion_time),area=sum(area)) %>%
+                st_transform(crs=4326) ## back to latlong
+
+            # The return value of `readLines()` is the actual value
+            # that will be returned in case there is no condition
+            # (e.g. warning or error).
+            # You don't need to state the return value via `return()` as code
+            # in the "try" part is not wrapped insided a function (unlike that
+            # for the condition handlers for warnings and error below)
+            return(pfilter)
+        },
+        error=function(cond) {
+            message("Problem with join. Possibly a topological error.")
+            message("Here's the original error message:")
+            message(cond)
+            # Choose a return value in case of error
+            return(0)
+        },
+        warning=function(cond) {
+            message("There was a warning:")
+            message("Here's the original warning message:")
+            message(cond)
+            # Choose a return value in case of warning
+            return(pfilter)
+        },
+        finally={
+        # NOTE:
+        # Here goes everything that should be executed at the end,
+        # regardless of success or error.
+        # If you want more than one expression to be executed, then you
+        # need to wrap them in curly brackets ({...}); otherwise you could
+        # just have written 'finally=<expression>'
+#            message(paste("Processed URL:", url))
+#            message("Some other message at the end")
+        }
+    )
+    return(out)
+}
+
+
 
 wmIn <- paste0(scratch,"/watermasks")
 
@@ -75,7 +129,12 @@ for(f in flist)
                                         #pfilter <- left_join(ids,psimpl) %>% st_as_sf %>% split(.$id_cogerh) %>% lapply(st_union) %>% do.call(c,.) %>% st_cast
             if(nrow(ids)>0)
             {
-              join_ids_with_feats(ids,psimpl)
+              pfilter=join_ids_with_feats(ids,psimpl)
+              if(pfilter!=0)
+              {
+                st_write(pfilter,paste0(wmIn,"/",fname,"_simplified.geojson"),driver="GeoJSON")
+              }
+
             } else cat("\n\nPolygons matching the COGERH watermask were not found in ",f,"\n")
         } else cat("\n\n The watermask was not found, check if scene is over the ocean or if there are no water bodies on the scene. \n\n")
     } else cat("\nAlready processed, jumping over simplify and filter ....\n")
@@ -87,59 +146,4 @@ for(f in flist)
         try(file.remove(paste0(wmIn,"/",fname,".gml")),silent=TRUE)
         try(file.remove(paste0(wmIn,"/",fname,".xsd")),silent=TRUE)
     }
-}
-
-
-join_ids_with_feats <- function(ids,psimpl) {
-    out <- tryCatch(
-        {
-            # Just to highlight: if you want to use more than one
-            # R expression in the "try" part then you'll have to
-            # use curly brackets.
-            # 'tryCatch()' will return the last evaluated expression
-            # in case the "try" part was completed successfully
-
-            message("This is the 'try' part")
-
-            pfilter <- left_join(ids,psimpl) %>%
-                st_as_sf %>%
-                group_by(id_cogerh) %>%
-                summarize(source_id=as.integer(first(source_id)),ingestion_time=first(ingestion_time),area=sum(area)) %>%
-                st_transform(crs=4326) ## back to latlong
-
-            st_write(pfilter,paste0(wmIn,"/",fname,"_simplified.geojson"),driver="GeoJSON")
-            # The return value of `readLines()` is the actual value
-            # that will be returned in case there is no condition
-            # (e.g. warning or error).
-            # You don't need to state the return value via `return()` as code
-            # in the "try" part is not wrapped insided a function (unlike that
-            # for the condition handlers for warnings and error below)
-            return(1)
-        },
-        error=function(cond) {
-            message("Problem with join. Possibly a topological error.")
-            message("Here's the original error message:")
-            message(cond)
-            # Choose a return value in case of error
-            return(0)
-        },
-        warning=function(cond) {
-            message("There was a warning:")
-            message("Here's the original warning message:")
-            message(cond)
-            # Choose a return value in case of warning
-            return(1)
-        },
-        finally={
-        # NOTE:
-        # Here goes everything that should be executed at the end,
-        # regardless of success or error.
-        # If you want more than one expression to be executed, then you
-        # need to wrap them in curly brackets ({...}); otherwise you could
-        # just have written 'finally=<expression>'
-#            message(paste("Processed URL:", url))
-#            message("Some other message at the end")
-        }
-    )
-    return(out)
 }
