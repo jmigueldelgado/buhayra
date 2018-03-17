64db6b25361bc5d5e954dc113a2935b0123dae0d
JM Delgado <jmigueldelgado@gmail.com>
2018-03-18 00:07:04 +0100

    corrects insert polygons

--- jobs/insert_polys_mongo.sh
+++ jobs/insert_polys_mongo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source activate mongodb
-cd /users/stud09/martinsd/proj/sar2watermask/modules
-python sar2watermask_cluster.py
+cd /users/stud09/martinsd/proj/sar2watermask
+python insertPolygons.py
