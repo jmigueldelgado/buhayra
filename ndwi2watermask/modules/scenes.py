af558e51cbdb3cf91dca3da18eb688225050a376
JM Delgado <jmigueldelgado@gmail.com>
2018-04-09 16:46:14 +0200

    get scenes for 2 projects at once

--- ndwi2watermask/modules/scenes.py
+++ ndwi2watermask/modules/scenes.py
@@ -5,8 +5,8 @@ import sys
 import os
 from modules.getpaths import *
 
-
 def getscenes():
+
     api = SentinelAPI(username, password, 'https://scihub.copernicus.eu/dhus')
 
     # download single scene by known product id
@@ -15,7 +15,7 @@ def getscenes():
     tf = datetime.now()
     # search by polygon, time, and SciHub query keywords
     footprint = geojson_to_wkt(read_geojson(home['parameters'] + '/extent_ce.geojson'))
-    products = api.query(footprint,
+    products_s2a = api.query(footprint,
                         date=(
                             date(t0.year,t0.month,t0.day),
                             date(tf.year,tf.month,tf.day)
@@ -25,4 +25,16 @@ def getscenes():
 
     # download all results from the search
     #s2aIn = '/home/delgado/Documents/tmp' # in case you are just testing
-    api.download_all(products,directory_path=s2aIn)
+    api.download_all(products_s2a,directory_path=s2aIn)
+
+    products_s1a = api.query(footprint,
+                         date=(
+                             date(t0.year,t0.month,t0.day),
+                             date(tf.year,tf.month,tf.day)
+                         ),
+                         producttype="GRD",
+                         platformname='Sentinel-1')
+
+
+        # download all results from the search
+    api.download_all(products_s1a,directory_path=sarIn)
