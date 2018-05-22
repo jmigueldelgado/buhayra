8d18ee03d9f1a5ca585f78a4c2bee0ec0291207c
JM Delgado <delgado@ubuntuserver.geoecology>
2018-05-22 16:39:06 +0200

    fixes versio nof miniconda

--- buhayra/jobs/5.2_funceme_api.sh
+++ buhayra/jobs/5.2_funceme_api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 exec 1>5.2_validation_funceme.log 2>&1
 
-source $HOME/local/miniconda2/bin/activate $HOME/local/miniconda2/envs/requests
+source $HOME/miniconda3/bin/activate $HOME/miniconda3/envs/requests
 
 python $HOME/proj/buhayra "update validation"
