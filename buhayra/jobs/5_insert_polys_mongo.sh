75ac161f9f0ce36629bbb9ab39e62cf4c5607689
Dr. rer. nat. José Miguel Martins Delgado <jose.martins.delgado@uni-potsdam.de>
2018-06-04 12:59:19 +0200

    removes duplicate call to write log file

--- buhayra/jobs/5_insert_polys_mongo.sh
+++ buhayra/jobs/5_insert_polys_mongo.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-exec 1>5_insert_polys_mongo.log 2>&1
-
 source /users/stud09/martinsd/local/miniconda2/bin/activate /users/stud09/martinsd/local/miniconda2/envs/mongodb
 
 python /users/stud09/martinsd/proj/buhayra "insert"
