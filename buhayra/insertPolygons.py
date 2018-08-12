2fd3a58e31baee0fd926832f120266e55c1b1e46
JM Delgado <jmigueldelgado@gmail.com>
2018-08-12 22:31:21 +0200

    includes $set to correct issue #17

--- buhayra/insertPolygons.py
+++ buhayra/insertPolygons.py
@@ -78,3 +78,28 @@ def testMongoConnect():
     s2w = db.sar2watermask ##  collection
     logger.debug(db.collection_names())
     print(db.collection_names())
+    logger.info("Connected to mongodb:")
+    logger.info("%s",s2w)
+
+    newlist = []
+    items=os.listdir(polOut)
+    for names in items:
+        if names.endswith('simplified.geojson'):
+            newlist.append(names)
+
+    for in_file in newlist[0]:
+        logger.info('\n inserting ' + in_file + ' in mongodb\n')
+
+        with open(polOut + '/' + in_file) as f:
+            data = geojson.load(f)
+
+        for feat in data["features"][0]:
+            dttm = datetime.strptime(feat["properties"]["ingestion_time"],"%Y/%m/%d %H:%M:%S+00")
+            feat["properties"]["ingestion_time"] = dttm
+            feat["properties"]["source_id"] = int(feat["properties"]["source_id"])
+
+            logger.info("Current feature's properties:")
+            logger.info("Ingestion Date:%s",feat["properties"]["ingestion_time"])
+            logger.info("ID:%s",feat["properties"]["id_funceme"])
+            feat_id = s2w.update_one({'properties.id_funceme':feat["properties"]["id_funceme"] , 'properties.ingestion_time' :feat["properties"]["ingestion_time"] },{'$set':feat},upsert=True).upserted_id
+            logger.info('Inserted feature ID: %s',feat_id)
