7090bfccae05356ec36baf362a8415bc829cd5fa
jmigueldelgado <jmigueldelgado@gmail.com>
2018-08-14 13:55:23 +0200

    more logging and gitignore

--- buhayra/getLatestPolygons.py
+++ buhayra/getLatestPolygons.py
@@ -9,9 +9,11 @@ from datetime import datetime
 from sshtunnel import SSHTunnelForwarder
 from buhayra.defAggregations import *
 from buhayra.getpaths import *
+import logging
 import subprocess as sp
 
 def connect_and_get(x):
+    logger = logging.getLogger('root')
     if socket.gethostname()!='ubuntuserver':
         server = SSHTunnelForwarder(
             MONGO_HOST,
@@ -20,11 +22,11 @@ def connect_and_get(x):
             remote_bind_address=('127.0.0.1', MONGO_PORT))
 
         server.start()
-        print("started ssh tunnel")
+        logger.info("started ssh tunnel")
 
         client = MongoClient('127.0.0.1', server.local_bind_port) # server.local_bind_port is assigned local port
     else:
-        print("connecting to local host")
+        logger.info("connecting to local host")
         client = MongoClient('mongodb://localhost:27017/')
 
 
@@ -39,7 +41,7 @@ def connect_and_get(x):
     ## get geojson standard feature collection
     feat_col=aggr2geojson(polys)
 
-    ## write
+    logger.info('writing out to ' + home['home']+'/load_to_postgis/latest.geojson')
     f=open(home['home']+'/load_to_postgis/latest.geojson','w')
     geojson.dump(feat_col,f)
     f.close()
