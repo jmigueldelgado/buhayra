5d16dce2541de2a640e728bc05af52f9f95aed3d
JM Delgado <jmigueldelgado@gmail.com>
2018-10-26 15:50:43 +0200

    thresholds with kittler.

--- sar2watermask/thresholding.py
+++ sar2watermask/thresholding.py
@@ -1,9 +1,27 @@
-import rasterio
 from buhayra.getpaths import *
 from os import listdir
+import numpy as np
+import numpy.ma as ma
+import rasterio
+import matplotlib.pyplot as plt
 
 f=selectTiff()
-dataset = rasterio.open(f)
+
+ds = rasterio.open(sarOut+'/'+f)
+
+r=ds.read(1)
+
+rmsk=ma.array(r*100000,mask= r<=0)
+thr=kittler(rmsk.astype(int))
+
+msk = rmsk>=thr
+wm=ma.array(rmsk,mask=msk)
+
+plt.imshow(wm)
+
+### polygonize, calc area and clean small features
+
+
 
 
 
@@ -13,10 +31,64 @@ def selectTiff():
         return None
     l=listdir(sarOut)
     import re
-    s=l[1]
     for s in l:
-        match=re.search('.tif',s)
-        match
+        if re.search('.tif$',s):
+            return(s)
+    return(s)
 
-    f=listdir(sarOut)[0]
-    return(f)
+
+
+
+
+
+def kittler(nparray):
+    """
+    Calculates minimum threshold
+    nparray: numpy array
+    return: threshold
+    """
+    # get indices of missing values
+    # and mask them
+
+    n = np.isnan(nparray)
+    band = np.ma.masked_array(nparray, mask=n)
+
+
+    # count entries in array
+    if band.count() < 50:
+        print("The size of the population is smaller than 50!\n")
+    else:
+        # calculate minimum and maximum as histogram breaks
+        breaks = [band.min(), np.ceil(band.max()) + 1]
+        # create sequence of min and max, determine length(=number of bins)
+        breaksSeq = np.arange(breaks[0], breaks[1], 5)
+
+        b = (breaksSeq[0], breaksSeq[-1])
+        bins = len(breaksSeq) - 1
+        # get density of each bin
+        density = np.histogram(band, bins=bins, range=b, density=True)[0]
+        g=np.arange(1,bins+1)
+        gg = [i**2 for i in g]
+
+        C = np.cumsum(density)
+        M = np.cumsum(density * g)
+        S = np.cumsum(density * gg)
+        sigmaF = np.sqrt(S / C - (M / C) ** 2)
+
+
+        Cb = C[len(g) - 1] - C
+        Mb = M[len(g) - 1] - M
+        Sb = S[len(g) - 1] - S
+        sigmaB = np.sqrt(np.abs(Sb / Cb - (Mb / Cb) ** 2))
+
+        P = C / C[len(g) - 1]
+
+        V = P * np.log(sigmaF) + (1 - P) * np.log(sigmaB) - P * np.log(P) - (1 - P) * np.log(1 - P)
+        V[np.isinf(V)] = np.nan
+
+        minV = np.nanmin(V)
+        for m in range(len(g)):
+            if V[m] == minV:
+                threshold = breaksSeq[m]
+
+        return threshold
