d83a58bc215ef1e4c6c5f8f7108b9dd3e2224fb2
JM Delgado <jmigueldelgado@gmail.com>
2018-11-07 13:49:51 +0100

    try `insertPolygons.py` from pbs job

--- buhayra/jobs/4_insert_polys_mongo.sh
+++ buhayra/jobs/4_insert_polys_mongo.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+#PBS -N 4_insert
+#PBS -M martinsd@uni-potsdam.de
+#PBS -j oe
+#PBS -l ncpus=1
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=24:00:00
+#PBS -l mem=5gb
+
 source activate fiorio_orson
 python /users/stud09/martinsd/proj/buhayra "insert"
