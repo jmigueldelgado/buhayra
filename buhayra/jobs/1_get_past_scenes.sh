626e149c9c7f03f09e8c3a6296534aa0617a678c
JM Delgado <delgado@ad.geoecology.uni-potsdam.de>
2018-12-02 20:42:09 +0100

    corrects cron jobs in order to start in conda evnironment

--- buhayra/jobs/1_get_past_scenes.sh
+++ buhayra/jobs/1_get_past_scenes.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 source activate buhayra
+
+touch $HOME/what_is_going_on
+
 python $HOME/proj/buhayra "get past scenes" $1 $2
--- buhayra/jobs/2_sar2watermask_year_month.sh
+++ buhayra/jobs/2_sar2watermask_year_month.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-source activate buhayra
-
 export JAVA_TOOL_OPTIONS="-Xmx30g"
 export _JAVA_OPTIONS="-Xmx32g -Xss2m"
 
-python $HOME/proj/buhayra "sar2sigma year month" $1 $2
+$HOME/local/miniconda3/envs/buhayra/bin/python $HOME/proj/buhayra "sar2sigma year month" $1 $2
