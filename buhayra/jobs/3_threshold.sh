e950ec2a4488c46e4234ddb1f8699685aba61532
JM Delgado <delgado@ad.geoecology.uni-potsdam.de>
2018-12-03 14:13:35 +0100

    new jobs for thresholding from compute server

---
+++ buhayra/jobs/3_threshold.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+$HOME/local/miniconda3/envs/dask/bin/python $HOME/proj/buhayra "threshold last" 1
---
+++ buhayra/jobs/3_threshold_yearmonth.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+$HOME/local/miniconda3/envs/dask/bin/python $HOME/proj/buhayra "threshold year month" $1 $2
