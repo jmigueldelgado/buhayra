f303fb102601bfb918c811a4e4be59a01d00ec6c
JM Delgado <jmigueldelgado@gmail.com>
2018-12-10 11:33:25 +0100

    successful testing of pca and scaling

--- buhayra/vegetatedwater.py
+++ buhayra/vegetatedwater.py
@@ -5,13 +5,14 @@ import logging
 import os
 import re
 import datetime
+from skimage.util.shape import view_as_windows, view_as_blocks
+from skimage.feature import greycomatrix, greycoprops
 
 def load_raster(f):
     with rasterio.open(vegIn + '/' + f,'r') as ds:
         r=ds.read(1)
         out_transform=ds.transform
     return r, out_transform
-
 
 def select_last_tiff():
     logger = logging.getLogger('root')
@@ -27,3 +28,27 @@ def select_last_tiff():
                 timestamp.append(datetime.datetime.strptime(scn.split('_')[4],'%Y%m%dT%H%M%S'))
         f=scenes[timestamp.index(max(timestamp))]
     return(f)
+
+def get_loadings_and_explained_variance(X,PCA):
+    pca = PCA(n_components=3)
+    pcafit=pca.fit(X)
+    eigenvectors = pcafit.components_
+    var = pcafit.explained_variance_ratio_
+    return eigenvectors, var
+
+
+def get_glcm_predictors(image):
+    window_shape=(3,3)
+    new_image = image[:-(image.shape[0]%window_shape[0]),:-(image.shape[1]%window_shape[1])]
+
+    B=view_as_blocks(new_image, window_shape)
+    X=B.reshape((B.shape[0]*B.shape[1],B.shape[2],B.shape[3]))
+    predictor=np.empty((X.shape[0],5))
+
+    for i in range(1000):#X.shape[0]):
+        glcm = greycomatrix(X[i,:,:], [1], [0, np.pi/4, np.pi/2, 3*np.pi/4, np.pi, 5*np.pi/4, 3*np.pi/2,2*np.pi], 256)
+        GLCM=np.sum(glcm,(2,3))
+        glcmi = [np.mean(GLCM),np.std(GLCM)**2,greycoprops(glcm, 'homogeneity')[0,0],greycoprops(glcm, 'dissimilarity')[0,0],greycoprops(glcm, 'correlation')[0, 0]]
+        predictor[i]=glcmi
+
+    return(predictor)
--- tests/test_glcm.py
+++ tests/test_glcm.py
@@ -1,75 +1,33 @@
-import matplotlib.pyplot as plt
-from skimage.feature import greycomatrix, greycoprops
-from skimage import data
-from sklearn.decomposition import PCA
-from skimage.util.shape import view_as_windows, view_as_blocks
 import numpy as np
 import buhayra.vegetatedwater as veggie
-
-f=veggie.select_last_tiff()
-
-r, out_transform = veggie.load_raster(f)
-
-##### test PCA in parallel
-# from dask_ml.decomposition import PCA
-
-
+import time
+from sklearn.preprocessing import scale
 from dask.distributed import Client, progress, LocalCluster
 import dask.array as da
-cluster = LocalCluster(processes=False,n_workers=1,threads_per_worker=8)
+from dask_ml.decomposition import PCA
+
+cluster = LocalCluster(processes=False,n_workers=1,threads_per_worker=3, memory_limit='2GB')
 client = Client(cluster)
 
-from sklearn import preprocessing
-from sklearn import datasets
-
-#open camera image
-#iris = datasets.load_iris()
-#XX = preprocessing.scale(iris.data)
-
-dX = da.from_array(r, chunks=(2000,2000))
+client.restart()
 
 
-dXscaled = dX.map_blocks(preprocessing.scale)
-dXscaled.compute() ## OR
-dXscaled.compute(schedule='single-threaded')
+f=veggie.select_last_tiff()
+r, out_transform = veggie.load_raster(f)
 
+r = np.random.rand(4000,4000)
+x = da.from_array(r, chunks=(500, 500))
 
-loadings,variance = get_loadings_and_explained_variance(XX,PCA)
-loadings[0,]
-variance
+# x = da.random.random((2000, 2000), chunks=(500, 500))
+start0=time.perf_counter()
+z=scale(x)
+time.perf_counter()-start0
 
-def get_loadings_and_explained_variance(X,PCA):
-    pca = PCA(n_components='mle')
-    pcafit=pca.fit(X)
-    eigenvectors = pcafit.components_
-    # np.linalg.norm(cmps[2,:])
-    var = pcafit.explained_variance_ratio_
-    return eigenvectors, var
+pca = PCA(n_components=2)
+pcafit=pca.fit(da.from_array(z,chunks=(500,500)))
+eigenvectors = pcafit.components_
 
+out=veggie.get_loadings_and_explained_variance(da.from_array(z,chunks=(500,500)),PCA)
 
-
-
-######  glcm on single cpu
-image = data.camera()
-
-
-import time
-start=time.process_time()
-predictors=get_glcm_predictors(image)
-time.process_time()-start
-
-def get_glcm_predictors(image):
-    window_shape=(3,3)
-    new_image = image[:-(image.shape[0]%window_shape[0]),:-(image.shape[1]%window_shape[1])]
-
-    B=view_as_blocks(new_image, window_shape)
-    X=B.reshape((B.shape[0]*B.shape[1],B.shape[2],B.shape[3]))
-    predictor=np.empty((X.shape[0],5))
-
-    for i in range(1000):#X.shape[0]):
-        glcm = greycomatrix(X[i,:,:], [1], [0, np.pi/4, np.pi/2, 3*np.pi/4, np.pi, 5*np.pi/4, 3*np.pi/2,2*np.pi], 256)
-        GLCM=np.sum(glcm,(2,3))
-        glcmi = [np.mean(GLCM),np.std(GLCM)**2,greycoprops(glcm, 'homogeneity')[0,0],greycoprops(glcm, 'dissimilarity')[0,0],greycoprops(glcm, 'correlation')[0, 0]]
-        predictor[i]=glcmi
-
-    return(predictor)
+client.close()
+cluster.close()
