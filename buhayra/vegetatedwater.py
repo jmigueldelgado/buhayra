mean and variance of glcm. needs to be checked and corrected!!

buhayra/vegetatedwater.py
@@ -47,9 +47,9 @@ def get_glcm_predictors(image):
     predictor=np.empty((X.shape[0],5))
 
     for i in range(1000):#X.shape[0]):
-        glcm = greycomatrix(X[i,:,:], [1], [0, np.pi/4, np.pi/2, 3*np.pi/4, np.pi, 5*np.pi/4, 3*np.pi/2,2*np.pi], 256)
+        glcm = greycomatrix(X[i,:,:], [1], [0, np.pi/4, np.pi/2, 3*np.pi/4], 256,symmetric=True)
         GLCM=np.sum(glcm,(2,3))
-        glcmi = [np.mean(GLCM),np.std(GLCM)**2,greycoprops(glcm, 'homogeneity')[0,0],greycoprops(glcm, 'dissimilarity')[0,0],greycoprops(glcm, 'correlation')[0, 0]]
+        glcmi = [np.mean(GLCM,axis=0)[0],np.std(GLCM)**2,greycoprops(glcm, 'homogeneity')[0,0],greycoprops(glcm, 'dissimilarity')[0,0],greycoprops(glcm, 'correlation')[0, 0]]
         predictor[i]=glcmi
 
     return(predictor)

tests/test_glcm.py
@@ -26,19 +26,29 @@ X_std = (x - np.amin(x)) / (np.amax(x) - np.amin(x))
 X_scaled = dask.array.round(X_std * (255 - 0) + 0)
 xuint = X_scaled.astype('uint8')
 get_glcm_predictors = dask.delayed(veggie.get_glcm_predictors)
-predictors = get_glcm_predictors(xuint)
+lazypredictors = get_glcm_predictors(xuint)
+predictors = compute(lazypredictors, scheduler='threads')
 pca = PCA(n_components=3)
-pcafit=pca.fit(predictors)
-results = compute(pcafit, scheduler='threads')
+
+i=time.time()
+
+
+
+pcafit=pca.fit(predictors[0])
+# results = compute(pcafit, scheduler='threads')
 time.time()-i
+
 
 eigenvectors = pcafit.components_
 
+# no dask
 
+f=veggie.select_last_tiff()
+with rasterio.open(vegIn + '/' +f,'r') as ds:
+    x=ds.read(1)
 
 # no dask
 i=time.time()
-x=np.random.random((10000,10000))
 X_std = (x - np.amin(x)) / (np.amax(x) - np.amin(x))
 X_scaled = np.round(X_std * (255 - 0) + 0)
 xuint = X_scaled.astype('uint8')