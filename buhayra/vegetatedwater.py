b318c3efd85c6b0f704472687c879f2f7201e450
JM Delgado <jmigueldelgado@gmail.com>
2018-12-19 12:06:35 +0100

    homogeneity, contrast, mean and variance

--- buhayra/vegetatedwater.py
+++ buhayra/vegetatedwater.py
@@ -38,9 +38,59 @@ def wrap_glcm_matrix(X,window_shape,levels):
         out[i,j,:,:]=np.sum(glcm,(2,3))
     return out
 
+def glcm_mean(matrix):
+    matrix=matrix.reshape(matrix.shape[0:2])
+    i = np.arange(matrix.shape[0])
+    return np.sum(i*matrix[i,0])
+
+def glcm_variance(matrix,glcm_mean_):
+    matrix=matrix.reshape(matrix.shape[0:2])
+    i = np.arange(matrix.shape[0])
+    return np.sum(matrix[i,0]*(i-glcm_mean_)**2)
+
+def wrap_glcm_mean(matrix):
+    h, w, leveli, levelj = matrix.shape
+    out=np.empty((h,w,1,1))
+    for i,j in np.ndindex(matrix[:,:,0,0].shape):
+        out[i,j,:,:]=glcm_mean(matrix[i,j,:,:].reshape(leveli,levelj,1,1))
+    return out
+
+def wrap_glcm_variance(matrix,glcm_mean_matrix):
+    h, w, leveli, levelj = matrix.shape
+    out=np.empty((h,w,1,1))
+    for i,j in np.ndindex(matrix[:,:,0,0].shape):
+        out[i,j,:,:]=glcm_variance(matrix[i,j,:,:].reshape(leveli,levelj,1,1),glcm_mean_matrix[i,j,0,0])
+    return out
+
 def wrap_glcm_dissimilarity(matrix):
     h, w, leveli, levelj = matrix.shape
     out=np.empty((h,w,1,1))
     for i,j in np.ndindex(matrix[:,:,0,0].shape):
-        out[i,j,:,:]=greycoprops(matrix[0,0,:,:].reshape(leveli,levelj,1,1), 'dissimilarity')[0,0]
+        out[i,j,:,:]=greycoprops(matrix[i,j,:,:].reshape(leveli,levelj,1,1), 'dissimilarity')[0,0]
     return out
+
+def wrap_glcm_contrast(matrix):
+    h, w, leveli, levelj = matrix.shape
+    out=np.empty((h,w,1,1))
+    for i,j in np.ndindex(matrix[:,:,0,0].shape):
+        out[i,j,:,:]=greycoprops(matrix[i,j,:,:].reshape(leveli,levelj,1,1), 'contrast')[0,0]
+    return out
+
+def wrap_glcm_homogeneity(matrix):
+    h, w, leveli, levelj = matrix.shape
+    out=np.empty((h,w,1,1))
+    for i,j in np.ndindex(matrix[:,:,0,0].shape):
+        out[i,j,:,:]=greycoprops(matrix[i,j,:,:].reshape(leveli,levelj,1,1), 'homogeneity')[0,0]
+    return out
+
+def wrap_glcm_matrix(X,window_shape,levels):
+    h, w, nrows, ncols = X.shape
+    out=np.empty((h,w,levels,levels))
+    for i,j in np.ndindex(X[:,:,0,0].shape):
+        glcm = greycomatrix(X[i,j,:,:], [1], [0, np.pi/4, np.pi/2, 3*np.pi/4], levels,normed=False,symmetric=True)
+        out[i,j,:,:]=np.sum(glcm,(2,3))
+    return out
+
+def wrap_mean(matrix):
+    matrix=matrix.reshape(matrix.shape[0:2])
+    return np.array([np.mean(matrix)])
--- tests/test_glcm.py
+++ tests/test_glcm.py
@@ -5,33 +5,12 @@ import dask.array as da
 from dask import compute, delayed
 import dask.threaded
 from skimage.util.shape import view_as_windows, view_as_blocks
-import buhayra.vegetatedwater as veggie
+from buhayra.vegetatedwater import *
 from skimage.feature import greycomatrix, greycoprops
-
-window_shape=(3,3)
-levels=256
+from distributed import Client,LocalCluster
 
 
-def wrap_glcm_dissimilarity(matrix):
-    h, w, leveli, levelj = matrix.shape
-    out=np.empty((h,w,1,1))
-    for i,j in np.ndindex(matrix[:,:,0,0].shape):
-        out[i,j,:,:]=greycoprops(matrix[0,0,:,:].reshape(leveli,levelj,1,1), 'dissimilarity')[0,0]
-    return out
-
-def wrap_glcm_matrix(X,window_shape,levels):
-    h, w, nrows, ncols = X.shape
-    out=np.empty((h,w,levels,levels))
-    for i,j in np.ndindex(X[:,:,0,0].shape):
-        glcm = greycomatrix(X[i,j,:,:], [1], [0, np.pi/4, np.pi/2, 3*np.pi/4], levels,normed=False,symmetric=True)
-        out[i,j,:,:]=np.sum(glcm,(2,3))
-    return out
-
-
-def wrap_mean(X):
-    return np.array([[np.mean(X)]])
-
-X = np.random.random((12, 12))*3
+X = np.random.random((240, 240))*3
 X=X.astype('uint')
 h, w = X.shape
 nrows=window_shape[0]
@@ -40,11 +19,15 @@ X=X.reshape(h//nrows, nrows,h*w//(ncols*nrows*(h//nrows)), ncols).swapaxes(1,2)
 
 i=time.time()
 matrix = wrap_glcm_matrix(X,window_shape,levels)
+diss=wrap_glcm_dissimilarity(matrix)
+glcm_mean_matrix=wrap_glcm_mean(matrix)
 time.time()-i
 
 
+window_shape=(3,3)
+levels=256
 
-dX = da.random.random((120, 120), chunks=(60, 60))*3
+dX = da.random.random((240, 240), chunks=(120, 120))*3
 excess=(dX.shape[0]%window_shape[0],dX.shape[1]%window_shape[1])
 dX = dX[0:(dX.shape[0]-excess[0]),0:(dX.shape[1]-excess[1])]
 dX=dX.astype('uint')
@@ -55,16 +38,34 @@ dX=dX.reshape(h//nrows, nrows,h*w//(ncols*nrows*(h//nrows)), ncols).swapaxes(1,2
 lazymatrix = da.map_blocks(wrap_glcm_matrix,dX,window_shape,levels,chunks=(10,10,levels,levels))
 ### persist lazy matrix here!!! http://distributed.dask.org/en/latest/memory.html#difference-with-dask-compute
 
-lazydiss = da.map_blocks(wrap_glcm_dissimilarity,lazymatrix,chunks=(20,20,1,1))
+
+
+c=LocalCluster(processes=False)#,n_workers=1,threads_per_worker=2)
+client = Client(c)
 
 i=time.time()
-predictor=compute(lazydiss, scheduler='threads')
+lazymatrix = client.persist(lazymatrix)
+lazydiss = da.map_blocks(wrap_glcm_dissimilarity,lazymatrix,chunks=(20,20,1,1))
+# lazycontrast = da.map_blocks(wrap_glcm_contrast,lazymatrix,chunks=(20,20,1,1))
+# lazyhomo = da.map_blocks(wrap_glcm_homogeneity,lazymatrix,chunks=(20,20,1,1))
+lazymean = da.map_blocks(wrap_glcm_mean,lazymatrix,chunks=(20,20,1,1))
+# lazyvar = da.map_blocks(wrap_glcm_variance,lazymatrix,lazymean,chunks=(20,20,1,1))
+diss=lazydiss.compute()
+# contrast=lazycontrast.compute()
+# homo=lazyhomo.compute()
+glcm_m=lazymean.compute()
+# glcm_v=lazyvar.compute()
 time.time()-i
 
-predictor[0].shape
+client.close()
+c.close()
 
 
 
+
+i=time.time()
+diss=compute(lazydiss, scheduler='threads')
+time.time()-i
 
 
 
@@ -88,11 +89,3 @@ def loadings_and_explained_variance(X,PCA):
         predictor[i]=glcmi
 
     return(predictor)
-
-def glcm_mean(matrix):
-    i = np.arange(matrix.shape[0])
-    return np.sum(i*matrix[i,0])
-
-def glcm_variance(matrix,glcm_mean_):
-    i = np.arange(matrix.shape[0])
-    return np.sum(matrix[i,0]*(i-glcm_mean_)**2)
