11a4cb5f6de97add9fcf3446017c68996cee39b2
JM Delgado <jmigueldelgado@gmail.com>
2019-01-11 01:20:16 +0100

    ammend

--- viz/folium_example.py
+++ viz/folium_example.py
@@ -1,11 +1,6 @@
 import folium
-import os
-import json
 
-reservoir = os.path.join('viz', 'latest-2-month.geojson')
 m = folium.Map(location=[-5, -38],zoom_start=8)
-
-folium.GeoJson(reservoir,name='geojson',tooltip=folium.features.GeoJsonTooltip(fields=['id_jrc'],aliases=['ID'],labels=True,localize=True,sticky=True)).add_to(m)
-
+folium.GeoJson('./viz/latest-2-month.geojson',name='geojson',tooltip=folium.features.GeoJsonTooltip(fields=['id_jrc'],aliases=['ID'],labels=True,localize=True,sticky=True)).add_to(m)
 folium.LayerControl().add_to(m)
 m.save('index.html')
