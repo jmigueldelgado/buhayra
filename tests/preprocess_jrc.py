4ac11b73e7e2de89d55ea6b9f630cd48043edb74
JM Delgado <jmigueldelgado@gmail.com>
2019-01-29 20:36:47 +0100

    preprocessing iberia

---
+++ tests/preprocess_jrc.py
@@ -0,0 +1,29 @@
+import rasterio
+from shapely.geometry import mapping, Polygon, shape
+from shapely.ops import cascaded_union, transform
+import os
+import pyproj
+import fiona
+
+os.listdir('/home/delgado/proj/buhayra/test_data')
+f= '/home/delgado/proj/buhayra/test_data/alentejo.tif'
+with rasterio.open(f,'r') as ds:
+    r=ds.read(1)
+    r[r>1]=1
+
+    with rasterio.open(f[:-4]+'_bin.tif','w',driver='GTiff',height=r.shape[0],width=r.shape[1],count=1,dtype=rasterio.int32,transform=ds.transform) as dsout:
+        dsout.write(r.astype(rasterio.int32),1)
+
+    polys=list()
+
+    for pol, value in features.shapes(r, transform=ds.transform):
+        if value==1:
+            polys.append(shape(pol))
+
+    jrc_featcoll = cascaded_union(polys)
+
+
+    project = partial(
+        pyproj.transform,
+        pyproj.Proj(init='epsg:4326'),
+        pyproj.Proj(init='epsg:32629'))
