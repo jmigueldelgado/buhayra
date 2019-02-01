a3020f2e5909de72b9fbf6517cbf990a943e63c2
JM Delgado <jmigueldelgado@gmail.com>
2019-02-01 11:38:39 +0100

    georeference

--- sar2watermask/sar.py
+++ sar2watermask/sar.py
@@ -190,7 +190,7 @@ def sar2sigma_265():
                     ProductIO.writeProduct(product_subset,os.path.join(targetdir,fname + "_locked"),outForm)
                     product_subset.dispose()
 
-                    # compress_tiff(os.path.join(targetdir,fname+'_locked.tif',os.path.join(targetdir,fname+'.tif')
+                    compress_tiff(os.path.join(targetdir,fname+'_locked.tif',os.path.join(targetdir,fname+'.tif')
                 else:
                     continue
 
