a801b299acfc9520b3aa281e66650f5c60cb0b80
JM Delgado <jmigueldelgado@gmail.com>
2019-10-14 23:12:00 +0200

    corrects conda env used in 4_rm_finished

--- buhayra/jobs/4_rm_finished.sh
+++ buhayra/jobs/4_rm_finished.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source $HOME/local/miniconda3/bin/activate buhayra
 
-$HOME/local/miniconda3/envs/dask/bin/python $HOME/proj/buhayra_sib "remove finished scenes"
+$HOME/local/miniconda3/envs/buhayra/bin/python $HOME/proj/buhayra_sib "remove finished scenes"
