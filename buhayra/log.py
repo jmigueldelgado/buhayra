545f6d0aaf00047935edc74b0efb4095269e0e0c
JM Delgado <delgado@ad.geoecology.uni-potsdam.de>
2021-03-05 11:28:21 +0100

    log files named after job names

--- buhayra/log.py
+++ buhayra/log.py
@@ -1,12 +1,13 @@
 import logging
 import os
+import sys
 
 pid=os.getpid()
 
 def setup_custom_logger(name,level="INFO"):
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
 
-    handler = logging.FileHandler('info' + str(pid) + '.log')
+    handler = logging.FileHandler('info' + sys.argv[1] + '.log')
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
